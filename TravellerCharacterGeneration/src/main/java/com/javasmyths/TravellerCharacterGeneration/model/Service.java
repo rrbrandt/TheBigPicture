72c1ecf8d28fb2af8162c4ec521327cdafc9028d
Richard <TheBigR314@gmail.com>
2022-03-29 17:50:52 -0400

    first

---
+++ TravellerCharacterGeneration/src/main/java/com/javasmyths/TravellerCharacterGeneration/model/Service.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2018 JavaSmyths javasmyths@javasmyths.com
+ */
+package com.javasmyths.travellercharactergeneration.model;
+
+/**
+ *
+ * @author Richard
+ */
+public enum Service {
+  Navy, Marines, Army, Scouts, Merchants, Other
+}
