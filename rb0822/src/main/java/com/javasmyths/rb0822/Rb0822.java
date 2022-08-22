a0c25d27c4089b90c9a7143f255ff119dfdda46c
Richard <TheBigR314@gmail.com>
2022-08-22 12:00:53 -0400

    new

---
+++ rb0822/src/main/java/com/javasmyths/rb0822/Rb0822.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2018 JavaSmyths javasmyths@javasmyths.com
+ */
+
+package com.javasmyths.rb0822;
+
+/**
+ *
+ * @author Richard
+ */
+public class Rb0822 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
